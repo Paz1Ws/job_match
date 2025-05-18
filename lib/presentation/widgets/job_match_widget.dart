c1d142dab5965469e5193e3ce6462a23a7e56fbe
4dominic888 <4dominin888@gmail.com>
2025-05-17 20:59:22 -0500

    adding tapable options

--- lib/presentation/widgets/job_match_widget.dart
+++ lib/presentation/widgets/job_match_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:job_match/config/constants/layer_constants.dart';
 
 class JobMatchWidget extends StatelessWidget {
@@ -6,15 +7,23 @@ class JobMatchWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: const [
-        CircleAvatar(
-          radius: kRadius20 + kRadius12,
-          backgroundImage: AssetImage('assets/images/job_match.jpg'),
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        splashColor: Colors.white.withOpacity(0.2),
+        hoverColor: Colors.white.withOpacity(0.1),
+        onTap: () => context.go('/homepage'),
+        child: Row(
+          children: const [
+            CircleAvatar(
+              radius: kRadius20 + kRadius12,
+              backgroundImage: AssetImage('assets/images/job_match.jpg'),
+            ),
+            SizedBox(width: 8),
+            Text('Job Match', style: TextStyle(color: Colors.white, fontSize: 20)),
+          ],
         ),
-        SizedBox(width: 8),
-        Text('Job Match', style: TextStyle(color: Colors.white, fontSize: 20)),
-      ],
+      ),
     );
   }
 }
